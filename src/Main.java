02b097e5cb55bdc88d4aa0a9ecfd5812528c2396
Lang <3583644902@qq.com>
2025-02-21 10:27:37 +0800

    my first commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ public class Main{
         "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
 
         System.out.println("Now playing the song");
-        mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 2);
-        
-        // --- TODO
+        mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 1);
+        MusicBox mbox = new MusicBox("S34TG65", 'Y', "Raindrops", "Misty", 
+        "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
+
+        System.out.println("Now playing the song");
+        mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 1);
 
         /* Repeat the above code to create another object  
          * Pass different parameters to the MusicBox constructor, but initialise premiumSong to N and when calling the
--- src/MusicBox.java
+++ src/MusicBox.java
@@ -18,7 +18,6 @@ public class MusicBox {
         this.isSongPremium = premiumSong;
         this.songArtists = artists;
         
-        // -- TODO
         /* Initialise  the rest of the variables in this Constructor block*/
     }
     
@@ -36,7 +35,6 @@ public class MusicBox {
         return isSongPremium;
     }
 
-    // -- TODO
         /* Create the rest of "getters" for the remaining variables */
     
         /* The following method is complete. You don't have to do anything here. Just try to understand it. */
